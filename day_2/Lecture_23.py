5af0c6231ffa4e4627145ab4efb6164c113c8300
mashkoor098 <patelmashkoor8@gmail.com>
2023-02-20 16:38:31 +0530

    lecture 23 & 25
    in this lecture I leaned about F string and type conversion.

---
+++ day_2/Lecture_23.py
@@ -0,0 +1,14 @@
+# Calculate the Body Mass Index (BMI) using Hight & wieght
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = int(weight) / float(height)**2
+
+# print(int(BMI))
+print("Your Body Mass Index is:  "+str(BMI))
+
